f7d0974b003739a4822d8963d8aca222369b8fde
LordAle5 <121577581+LordAle5@users.noreply.github.com>
2024-08-26 11:54:43 -0500

    Update class1.cpp

--- class1.cpp
+++ class1.cpp
@@ -38,3 +38,74 @@ int main(int argc, char** argv) {
     cout<<dec;
     return 0;
 }
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    int dia,mes,anio,num1=1256;
+    dia=1;
+    mes=1;
+    anio=2024;
+    
+    cout<<dia<<"/"<<mes<<"/"<<anio<<endl;
+    cout<<right<<setw(2)<<dia<<"/"<<setw(2)<<mes<<"/"<<anio<<endl;
+    cout.fill('0');
+    //a partir de esta linea todo se llena con cero
+    cout<<right<<setw(2)<<dia<<"/"<<setw(2)<<mes<<"/"<<anio<<endl;
+    cout<<setw(10)<<num1<<endl;
+    cout.fill(' ');
+    cout<<right<<setw(2)<<dia<<"/"<<setw(2)<<mes<<"/"<<anio<<endl;
+    //solo para esa linea
+    cout<<setfill('0')<<setw(2)<<dia<<"/"<<setw(2)<<mes<<"/"<<anio<<endl;
+    
+    return 0;
+}
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    double real1=12.5667,real2=123.6,real3=2424234.4;
+    cout<<real1<<endl<<real2<<endl<<real3<<endl;
+    cout<<setw(15)<<real1<<endl<<setw(15)<<real2<<endl<<setw(15)<<real3<<endl;
+    cout<<"Precision 5"<<endl;
+    cout.precision(5);
+    cout<<fixed<<setw(15)<<real1<<endl<<setw(15)<<real2<<endl<<setw(15)<<real3
+            <<endl;
+    cout<<setprecision(5)<<fixed<<setw(15)<<real1<<endl<<setw(15)<<real2<<endl
+            <<setw(15)<<real3<<endl;
+    return 0;
+}
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout<<"REPORTE DE ALUMNOS"<<endl;
+    cout<<setw(65)<<"REPORTE DE ALUMNOS"<<endl<<endl;
+    cout<<setw(20)<<"CODIGO"<<setw(38)<<"NOMBRE"<<setw(45)<<"MONTO A PAGAR"<<endl;
+    cout<<setw(21)<<"20233429"<<setw(39)<<"Alejandro"<<setw(41)<<"10 soles"<<endl;
+    return 0;
+}
